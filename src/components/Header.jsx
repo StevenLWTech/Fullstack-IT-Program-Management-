85e26372d46dff90252eae82602e191cbe5f05a7
Steven <s-steven.truong@lwtech.edu>
2023-05-11 15:08:46 -0700

    InitialCommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import headerImg from './images/Header.png'
+export default function Header() {
+  return (
+    <div className='header'><img src={headerImg} alt="" /></div>
+    
+  )
+}
