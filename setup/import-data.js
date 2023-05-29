5ce88a20c0e437a23301dcaa86ae285cb7e1cae3
aleksandar <alpe88.dev@gmail.com>
2023-05-29 12:49:52 -0700

    various updates
    removed header and footer plus sort icons updates

--- setup/import-data.js
+++ setup/import-data.js
@@ -1,59 +1,76 @@
-const xlsx = require('xlsx');
-const { Pool } = require('pg');
+const xlsx = require("xlsx");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'Pr@shant425',
-  database: 'postgres'
+  host: "localhost",
+  port: 1188,
+  user: "postgres",
+  password: "acidrain",
+  database: "sql_demo",
 });
 
-async function main() {
+async function dropAndImportData() {
   try {
+    await pool.query("DROP TABLE IF EXISTS mytable");
+
     await pool.connect();
 
     const createTableQuery = `
       CREATE TABLE IF NOT EXISTS mytable (
         id serial PRIMARY KEY,
         "College" text COLLATE pg_catalog."default",
-        "Program Type" text COLLATE pg_catalog."default",
+        "featured_image" text COLLATE pg_catalog."default",
         "Program Name" text COLLATE pg_catalog."default",
         "Category" text COLLATE pg_catalog."default",
+        "Program Type" text COLLATE pg_catalog."default",
         "Region" text COLLATE pg_catalog."default",
-        "Hyperlink" text COLLATE pg_catalog."default"
+        "HyperLink" text COLLATE pg_catalog."default"
       );
     `;
 
     await pool.query(createTableQuery);
 
-    const workbook = xlsx.readFile('sbctc.xlsx');
-    const worksheet = workbook.Sheets['IT Prof Tech Programs'];
+    const workbook = xlsx.readFile("sbctc2.xlsx");
+    const worksheet = workbook.Sheets["Sheet1"];
     const data = xlsx.utils.sheet_to_json(worksheet);
 
     for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
       const row = data[rowIndex];
 
-      // Check for hyperlink in column B
-      const hyperlinkCellB = worksheet[`B${rowIndex + 1}`];
-      const hyperlinkB = hyperlinkCellB && hyperlinkCellB.l && hyperlinkCellB.l.display ? hyperlinkCellB.l.display : "";
+      // Check for hyperlink in column D
+      const hyperlinkObject = worksheet[`D${rowIndex + 1}`];
+      const url =
+        hyperlinkObject && hyperlinkObject.l && hyperlinkObject.l.Target
+          ? hyperlinkObject.l.Target
+          : "";
 
       // Check for hyperlink in column C
       const hyperlinkCellC = worksheet[`C${rowIndex + 1}`];
-      const hyperlinkC = hyperlinkCellC && hyperlinkCellC.l && hyperlinkCellC.l.display ? hyperlinkCellC.l.display : "";
+      const hyperlinkC =
+        hyperlinkCellC && hyperlinkCellC.l && hyperlinkCellC.l.display
+          ? hyperlinkCellC.l.display
+          : "";
 
       // Choose the hyperlink that's not empty
-      const hyperlink = hyperlinkC || hyperlinkB || "";
-      console.log(row['Program Type'] + " " + hyperlink);
+      const hyperlink = url || hyperlinkC || "";
+      console.log(row["Program Type"] + " " + hyperlink);
       const insertQuery = {
-        text: 'INSERT INTO mytable ("College", "Program Type", "Program Name", "Category", "Region", "Hyperlink") VALUES ($1, $2, $3, $4, $5, $6)',
-        values: [row['College'], row['Program Type'], row['Program Name'], row['Category'], row['Region'], hyperlink]
+        text: 'INSERT INTO mytable ("College", "featured_image","Program Name","Category", "Program Type","Region", "HyperLink") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        values: [
+          row["College"],
+          row["featured_image"],
+          row["Program Name"],
+          row["Category"],
+          row["Program Type"],
+          row["Region"],
+          hyperlink,
+        ],
       };
 
       await pool.query(insertQuery);
     }
 
-    console.log('Data imported successfully');
+    console.log("Data imported successfully");
   } catch (err) {
     console.error(err);
   } finally {
@@ -61,4 +78,4 @@ async function main() {
   }
 }
 
-main();
+dropAndImportData();
