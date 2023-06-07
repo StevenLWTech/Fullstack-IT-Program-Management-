a700e667b163b4a91246a856c76d4b4b3acf28c1
StevenLWTech <s.steven-lwtech.edu>
2023-06-07 13:47:38 -0700

    changed endpoint

--- src/App.js
+++ src/App.js
@@ -10,41 +10,41 @@ function App() {
   const [data, setData] = useState(null);
 
   // Using Aleksander's endpoint
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        "https://www.coeforict.org/wp-json/college_programs/v1/college-programs"
-      );
-      // const responseData = response.data;
-      const responseData = response.data.map((item) => {
-        return {
-          id: item.uuid|| "",
-          College: item.college,
-          Category: item.category|| "",
-          "Program Type": item.program_type || "",
-          "Program Name": item.program_name|| "",
-          Region: item.region|| "",
-          HyperLink: item.hyperlink,
-        };
-      });
-      setData(responseData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  // localhost endpoint
   // const fetchData = async () => {
   //   try {
-  //     const response = await axios.get("http://localhost:8000/api/data");
-
-  //     const responseData = response.data;
-
+  //     const response = await axios.get(
+  //       "https://www.coeforict.org/wp-json/college_programs/v1/college-programs"
+  //     );
+  //     // const responseData = response.data;
+  //     const responseData = response.data.map((item) => {
+  //       return {
+  //         id: item.uuid|| "",
+  //         College: item.college,
+  //         Category: item.category|| "",
+  //         "Program Type": item.program_type || "",
+  //         "Program Name": item.program_name|| "",
+  //         Region: item.region|| "",
+  //         HyperLink: item.hyperlink,
+  //       };
+  //     });
   //     setData(responseData);
   //   } catch (error) {
   //     console.error("Error fetching data:", error);
   //   }
   // };
+
+  // localhost endpoint
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/data");
+
+      const responseData = response.data;
+
+      setData(responseData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
