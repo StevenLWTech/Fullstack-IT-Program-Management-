8f8e6868af7fb5e044ce13e2a5585b41160ebb07
StevenLWTech <s.steven-lwtech.edu>
2024-01-20 19:42:08 -0800

    update sql login info

--- setup/server.js
+++ setup/server.js
@@ -7,8 +7,6 @@ const { Pool } = require("pg");
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -26,9 +24,9 @@ app.use((req, res, next) => {
 const pool = new Pool({
   host: "localhost",
   port: 1188,
-  user: "postgres",
-  password: "acidrain",
-  database: "sql_demo",
+  user: "your_user",
+  password: "your_password",
+  database: "your_database",
 });
 
 const port = 9000;
